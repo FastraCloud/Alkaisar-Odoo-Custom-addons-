66de909e7247ff72285306e86d63a58a38510059
thir13ten <estainmakaudze@gmail.com>
2023-11-06 11:06:20 +0200

    ADD PURCHASE REQUST ID IN ROF ADD COMMISIONS, FX IN SALES ORDER

--- tsc_customization/__openerp__.py
+++ tsc_customization/__openerp__.py
@@ -1,32 +1,26 @@
 # -*- coding: utf-8 -*-
 {
     'name': "tsc_customization",
-
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
     'description': """
         Long description of module's purpose
     """,
-
-    'author': "My Company",
+    'author': "Estain Makadze",
     'website': "http://www.yourcompany.com",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    'depends': ['base','sale','purchase'],
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/sales_views_customs.xml',
+        'views/purchase_views_customs.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
